ffe3ccfaf1660fd52794f2df2daea569e203821d
Jitendra Pal <55188975+threej-in@users.noreply.github.com>
2022-01-01 17:28:08 +0530

    Create footer.php

---
+++ src/page/footer.php
@@ -0,0 +1,82 @@
+<body>
+        <body>
+        <div class="bar">  
+            <div class="head">
+               <div class="headnev">    
+                 <ul>
+                     <li class="log"><a href='#'>VOTTING SYSTEM</a></li>
+                     <div class="mnu">
+                       <li><a href="#">Menu</a></li>
+                     </div>
+                     <li><a href="#"><b>Home</b></a></li>
+                     <li><a href="#"><b>page 1</b></a></li>
+                     <li><a href="#"><b>page 2</b></a></li>
+                   </ul>
+               </div>
+            </div>
+            <div class="sidnev">
+                <div class="prof">
+                    <img src="wallpapersden.com_madara-uchiha-anime_2384x1342.jpg" alt="">
+                </div>
+                <h3>USER Name   </h3>
+                <ul>
+                  <li><a href="votpoll.html">Creat Poll</a></li>
+                  <li><a href="">Edit Poll</a></li>
+                  <li><a href="">DeLete Poll</a></li>
+                  <li><a href="">setting</a></li>
+                </ul>
+            </div>
+            </div>
+            <h3><u>Active Polls</u></h3>
+            <div class='headbox'>
+                <samp class='box'>
+                    <p> poll: 1</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 2</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 3</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 4</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 5</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 6</p>
+                </samp>
+               
+            </div>
+
+            <h3><u>Active Polls</u></h3>
+            <div class='headbox'>
+                <samp class='box'>
+                    <p> poll: 1</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 2</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 3</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 4</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 5</p>
+                </samp>
+                <samp class='box'>
+                    <p> poll: 6</p>
+                </samp>
+               
+            </div>
+            <script>
+                var menu = document.querySelector(".mnu");
+                menu.addEventListener("click",function(){
+                    document.querySelector("body").classList.toggle("active");  
+                })
+            </script>
+    </body>
+</html>
