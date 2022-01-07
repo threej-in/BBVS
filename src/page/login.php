acbb6fc0386ac5c678b07bca5905d5b4885c128c
ParthMithapara <87018787+ParthMithapara@users.noreply.github.com>
2022-01-07 11:10:36 +0530

    Add files via upload

--- src/page/login.php
+++ src/page/login.php
@@ -16,20 +16,24 @@
         flex-flow: column;
         row-gap: 1em;
     }
+    .error{
+        color:red;
+        font-size:14px;
+    }
 </style>
 <div class="login-form">
-    <form action="#" method="POST">
+    <form action="#" method="POST" id="login">
         <div>
             <h2>Log In</h2>
             <p class="sm">Don't have an account? Register <a href="/register.php" class="bluetext">here</a></p>
         </div>
         <section>
             <label for="loginid">Username or Email</label>
-            <input type="text" name="loginid" id="" placeholder="Enter your username or email address..." required>
+            <input type="text" name="loginid" id="loginid" placeholder="Enter your username or email address...">
         </section>
         <section>
             <label for="loginid">Password</label>
-            <input type="password" name="password" id="" placeholder="Enter your password" required>
+            <input type="password" name="password" id="password" placeholder="Enter your password">
         </section>
         <button type="submit" class="blue">Login</button>
     </form>
